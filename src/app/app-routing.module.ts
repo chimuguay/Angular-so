49ee06068ebc968c38bdc8898c323730f3a5f064
chimuguay <171255@ids.upchiapas.edu.mx>
2019-12-05 15:14:53 -0600

    Nueva3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { UsersComponent } from './components/users/users.component';
+import { RegisterComponent } from './components/register/register.component';
 
 
 const routes: Routes = [
-  {path: 'home', component: UsersComponent},
-	{path: 'login', component: LoginComponent},
+  {path: 'home', component: RegisterComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'buscar', component: UsersComponent},
 
 ];
 
